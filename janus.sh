62ef0f1d02e5c7276a428d538075d13205e95b31
GRAPE-POWERED\Grape Flavoured <themdisguised@protonmail.com>
2023-09-21 22:54:26 +0100

    added manual compile support for Janus - just to have it in the script if you need a specific version

--- janus.sh
+++ janus.sh
@@ -36,8 +36,42 @@ setup_apt() {
 }
 
 setup_manual() {
-  apt update
-# magic dust here
+# Confirmed working on 20.04 - haven't tested this script but manually this works. 
+  apt-get install -y apt-utils libmicrohttpd-dev libjansson-dev libssl-dev libsofia-sip-ua-dev libglib2.0-dev libopus-dev libogg-dev libusrsctp1 libusrsctp-dev libcurl4-openssl-dev liblua5.3-dev libconfig-dev pkg-config gengetopt libtool automake autoconf cmake gtk-doc-tools libini-config-dev libcollection-dev autotools-dev make git doxygen graphviz ffmpeg python3-pip sudo
+# pip install of meson & Ninja
+  pip3 install meson ninja
+# lib websockets
+  mkdir janus-build
+  cd janus-build
+  git clone https://libwebsockets.org/repo/libwebsockets
+  cd libwebsockets
+  mkdir build
+  cd build
+  cmake -DLWS_MAX_SMP=1 -DCMAKE_INSTALL_PREFIX:PATH=/usr -DCMAKE_C_FLAGS="-fpic" ..
+  make && sudo make install
+  cd ../..
+# lib nice
+  git clone https://gitlab.freedesktop.org/libnice/libnice
+  cd libnice
+  meson --prefix=/usr build && ninja -C build && sudo ninja -C build install
+  cd ..
+# libsrtp2
+  wget https://github.com/cisco/libsrtp/archive/v2.2.0.tar.gz
+  tar xfv v2.2.0.tar.gz
+  cd libsrtp-2.2.0
+  ./configure --prefix=/usr --enable-openssl
+  make shared_library && sudo make install
+  cd ..
+# Janus:
+  git clone https://github.com/meetecho/janus-gateway.git
+  cd janus-gateway
+  sh autogen.sh
+  ./configure --prefix=/opt/janus
+  make
+  make install
+  mkdir /var/log/janus
+  cd /opt/janus/bin
+  ./janus --version
 }
 
 reboot_selection() {
